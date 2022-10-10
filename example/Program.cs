0b2f0242e45ab7b2df9befaf364f9bd369ae556e
Benjamin Giesinger <benjamin.giesinger@pm.me>
2022-10-10 12:27:35 +0200

    Added localization

--- example/Program.cs
+++ example/Program.cs
@@ -1,3 +1,4 @@
+using DnDCharacterSheets;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ExampleApplication;
@@ -8,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSingleton<SessionStorage, SessionStorage>();
+builder.Services.AddDnDCharacterSheets();
 
 await builder.Build().RunAsync();
---
+++ src/DnDCharacterSheetExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace DnDCharacterSheets;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+
+public static class DnDCharacterSheetExtensions
+{
+    public static IServiceCollection AddDnDCharacterSheets(this IServiceCollection serviceCollection)
+    {
+        serviceCollection
+            .AddSingleton<IStringLocalizerFactory, ResourceManagerStringLocalizerFactory>()
+            .AddScoped(typeof(IStringLocalizer<>), typeof(StringLocalizer<>));
+        return serviceCollection;
+    }
+}
